ea289f29a5cc47eebd22274ebc4440bae5ac02a4
Alla777 <allasandra1725@gmail.com>
2019-04-19 17:30:18 +0300

    add logo

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import logo from './logo.png'
+
 const Logo = () =>{
     return(
-        <div>
-
+        <div className='logo'>
+            <a href=""><img src={logo} alt=""/></a>
         </div>
     )
 }
