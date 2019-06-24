028c348c663742b12013311f208d8555ae84cae7
Alla777 <allasandra1725@gmail.com>
2019-06-24 20:29:36 +0300

    Updates

---
+++ precache-manifest.40b23c22cf4ad5d088114f9f7f3e0f9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e92eafbeffaa2abb840a4de170c706",
+    "url": "/NewsSiteReact/static/media/imageSide2.d8e92eaf.png"
+  },
+  {
+    "revision": "461974904d41f9ccdd8b",
+    "url": "/NewsSiteReact/static/css/main.6edf35d1.chunk.css"
+  },
+  {
+    "revision": "0724b5c0411072678cb7",
+    "url": "/NewsSiteReact/static/js/runtime~main.43963c1f.js"
+  },
+  {
+    "revision": "d397a981cf07af543bb0",
+    "url": "/NewsSiteReact/static/js/2.3ae741d6.chunk.js"
+  },
+  {
+    "revision": "b5341cb67fadb8e1a76b9d40cdd69432",
+    "url": "/NewsSiteReact/static/media/imageSide1.b5341cb6.png"
+  },
+  {
+    "revision": "461974904d41f9ccdd8b",
+    "url": "/NewsSiteReact/static/js/main.c277fdae.chunk.js"
+  },
+  {
+    "revision": "8584b6c739c7b2d91ead525ac2fbede7",
+    "url": "/NewsSiteReact/static/media/imageSide3.8584b6c7.png"
+  },
+  {
+    "revision": "f6f7e4a8f2a7d4700489e70515c0f6c1",
+    "url": "/NewsSiteReact/static/media/imageSide4.f6f7e4a8.png"
+  },
+  {
+    "revision": "65bd06f6b35feb22e132e32da914121e",
+    "url": "/NewsSiteReact/static/media/footer-background.65bd06f6.png"
+  },
+  {
+    "revision": "fc52c6c67d3708ae667f6c50b71f2ef6",
+    "url": "/NewsSiteReact/static/media/layer.fc52c6c6.png"
+  },
+  {
+    "revision": "11264df8d57d3842f38e945e44ce5cbb",
+    "url": "/NewsSiteReact/index.html"
+  }
+];
