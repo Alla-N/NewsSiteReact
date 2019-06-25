9a881ea3a7ad5d0bcf839e7f58ae2bcf471074dd
Alla777 <allasandra1725@gmail.com>
2019-06-25 12:30:41 +0300

    add  hush router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -13,7 +13,7 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}>
-    <BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+    <HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
