6d5544179782676178e2af2740420b0f639cc5b4
Alla777 <allasandra1725@gmail.com>
2019-06-25 12:57:07 +0300

    test img link

--- src/App/Main/MainMain/news.js
+++ src/App/Main/MainMain/news.js
@@ -5,7 +5,7 @@ const news=[
 {
     id:1,
     tag:'lifestyle',
-    image:'/images/FullSize_img.jpg',
+    image:'images/fullsize_img.jpg',
     title: '10 SKILLS That Are HARD to Learn, BUT Will Pay Off FOREVER!',
 
     shortDescription:'The one constant thing in our life is change. We cannot avoid it and the more we resist change the tougher our life becomes. I know this because I was very skilled at deflecting change in life.',
